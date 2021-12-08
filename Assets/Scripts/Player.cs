005617701ad643a691f4ac7e62032efd14231e05
Etienne Javelle <75083138+EtienneJavelle@users.noreply.github.com>
2021-12-08 17:19:18 +0100

    Level 3 player

--- Assets/Scripts/Player.cs
+++ Assets/Scripts/Player.cs
@@ -10,36 +10,45 @@ public class Player : Etienne.Singleton<Player> {
 
     private void Start() {
         ball = GetComponentInChildren<Ball>();
-       if(!debug) SetTurn(false);
+        if(!debug) {
+            SetTurn(false);
+        }
     }
 
     private void Update() {
-        if(isYourTurn && Input.GetKeyDown(KeyCode.Alpha1)) {
-            if(levelCam.Priority == 11) {
-                levelCam.Priority = 9;
-            } else {
-                levelCam.Priority = 11;
+        if(isYourTurn) {
+            if(Input.GetKeyDown(KeyCode.Alpha1)) {
+                if(levelCam.Priority == 11) {
+                    levelCam.Priority = 9;
+                } else {
+                    levelCam.Priority = 11;
+                }
+            }
+            if(Input.GetKeyDown(KeyCode.R)) {
+                ball.transform.localPosition = Vector3.zero;
+                ball.GetComponent<Rigidbody>().velocity = Vector3.zero;
+                Connect.Send(nameof(MessageType.Update), ball.transform.position.x, ball.transform.position.y, ball.transform.position.z);
             }
         }
     }
 
-    public void SetTurn(bool isYourTurn)
-    {
+    public void SetTurn(bool isYourTurn) {
         this.isYourTurn = isYourTurn;
         hasPlayed = !isYourTurn;
         ball.enabled = isYourTurn;
         levelCam.Priority = isYourTurn ? 9 : 11;
 
-        if (isYourTurn)
+        if(isYourTurn) {
             StartCoroutine(SendUpdate());
+        }
     }
 
-    private IEnumerator SendUpdate()
-    {
-        while (ball.enabled)
-        {
-            if (hasPlayed)
+    private IEnumerator SendUpdate() {
+        while(ball.enabled) {
+            if(hasPlayed) {
                 Connect.Send(nameof(MessageType.Update), ball.transform.position.x, ball.transform.position.y, ball.transform.position.z);
+            }
+
             yield return new WaitForSeconds(0.0167f);
         }
     }
