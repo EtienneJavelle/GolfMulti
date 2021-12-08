03c33a9c082ef4e9b068a927e1c8922ec94e737d
Geoffrey Bernard <geoffreey.bernard@gmail.com>
2021-12-08 11:51:17 +0100

    Club Animation

--- Assets/Scripts/Ball.cs
+++ Assets/Scripts/Ball.cs
@@ -4,14 +4,20 @@ public class Ball : MonoBehaviour
 {
     private Rigidbody rgbd;
 
-    [SerializeField] private float minForce = 2f;
-    [SerializeField] private float maxForce = 10f;
+    [SerializeField] private float minForce = 0.2f;
+    [SerializeField] private float maxForce = 4f;
 
     [SerializeField] private float force = 0f;
 
+    [SerializeField] private float minClubDist = 0.05f;
+    [SerializeField] private float maxClubDist = 0.25f;
+
+    [SerializeField] private float clubDist = 0f;
+
     [SerializeField] private bool loading = false;
 
-    [SerializeField] private GameObject arrow;
+    [SerializeField] private Transform arrow;
+    [SerializeField] private Transform club;
 
     private void Awake()
     {
@@ -20,38 +26,52 @@ public class Ball : MonoBehaviour
 
     private void Start()
     {
-        
+        ResetLoad();
     }
 
     private void Update()
     {
         if (Input.GetMouseButtonUp(1))
-        {
-            loading = false;
-
-            force = minForce;
-        }
+            ResetLoad();
         else if (Input.GetMouseButtonDown(0))
-        {
-            loading = true;
-
-            force = minForce;
-        }
+            ResetLoad(true);
         else if (Input.GetMouseButtonUp(0) && loading)
         {
-            loading = false;
-
             rgbd.AddForce(arrow.transform.forward * force, ForceMode.Impulse);
 
-            force = minForce;
+            ResetLoad();
         }
         
         if (loading)
             force = Mathf.Lerp(force, maxForce, Time.deltaTime);
 
-        arrow.transform.position = transform.position;
-        arrow.transform.rotation = Quaternion.Euler(0f, Camera.main.transform.rotation.eulerAngles.y, 0f);
+        MoveOnForward(arrow);
+        arrow.position += arrow.forward * 0.5f;
 
-        arrow.transform.position += arrow.transform.forward * 0.5f;
+        MoveOnForward(club);
+
+        float load = 1f / maxForce * force;
+        clubDist = Mathf.MoveTowards(clubDist, minClubDist + load * (maxClubDist - minClubDist), 10f * Time.deltaTime);
+
+        club.position -= club.forward * clubDist;
+
+        rgbd.angularVelocity = Vector3.MoveTowards(rgbd.angularVelocity, Vector3.zero, 10f * Time.deltaTime);
+    }
+
+    private void ResetLoad(bool loading = false)
+    {
+        this.loading = loading;
+
+        arrow.gameObject.SetActive(loading);
+        club.gameObject.SetActive(loading);
+
+        force = minForce;
+        clubDist = minClubDist;
+    }
+
+    private void MoveOnForward(Transform other)
+    {
+        other.position = transform.position;
+        other.rotation = Quaternion.Euler(0f, Camera.main.transform.rotation.eulerAngles.y, 0f);
     }
 }
